4cf786841172e1b91fce26cbb70dd0cb6d6a3c3d
TrofimovAssya <petitkalimero@gmail.com>
2020-03-05 15:15:04 -0500

    added benchmark model

--- datasets.py
+++ datasets.py
@@ -9,7 +9,7 @@ import pandas as pd
 class TCRDataset(Dataset):
     """TCR CDR3 dataset"""
 
-    def __init__(self,root_dir='.',save_dir='.', data_file='data.npy'):
+    def __init__(self,root_dir='.',save_dir='.', data_file='data.npy', nb_kmer = 1000):
         self.root_dir = root_dir
         data_path = os.path.join(root_dir, data_file)
         self.data = pd.read_csv(data_path, header=None)
@@ -19,8 +19,34 @@ class TCRDataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, idx):
-        tcr = np.load(f'{self.root_dir}/{idx}_tcr_gd.npy')
+        tcr = np.load(f'{self.root_dir}/{idx}_tcr.npy')
+        
         return tcr
+
+    def input_size(self):
+        return self.nb_kmer
+
+    def extra_info(self):
+        info = OrderedDict()
+        return info
+
+class TCRBenchmark(Dataset):
+    """TCR CDR3 dataset"""
+
+    def __init__(self,root_dir='.',save_dir='.', data_file='data.npy', nb_kmer = 1000):
+        self.root_dir = root_dir
+        data_path = os.path.join(root_dir, data_file)
+        self.data = pd.read_csv(data_path, header=None)
+        self.data = list(self.data[0])
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        tcr = np.load(f'{self.root_dir}/{idx}_gd.npy')
+        target = np.load(f'{self.root_dir}/{idx}_targets.npy')
+        
+        return tcr, target
 
     def input_size(self):
         return self.nb_kmer
@@ -34,7 +60,9 @@ class TCRDataset(Dataset):
 def get_dataset(opt, exp_dir):
 
     if opt.dataset == 'tcr':
-        dataset = TCRDataset(root_dir=opt.data_dir, save_dir =exp_dir,data_file = opt.data_file)
+        dataset = TCRDataset(root_dir=opt.data_dir, save_dir =exp_dir,data_file = opt.data_file, nb_kmer = opt.nb_kmer)
+    elif opt.dataset == 'tcr_benchmark':
+        dataset = TCRBenchmark(root_dir=opt.data_dir, save_dir =exp_dir,data_file = opt.data_file, nb_kmer = opt.nb_kmer)
     else:
         raise NotImplementedError()
 
